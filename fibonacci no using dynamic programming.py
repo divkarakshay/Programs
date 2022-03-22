a3f5cf0f010511ff885ae7067efb846ce35a9c87
divkarakshay <divkarakshay@gmail.com>
2022-03-22 11:35:28 +0530

    key error

---
+++ fibonacci no using dynamic programming.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+def fib(n,memoization ={}):
+  
+  if n in memoization:
+    return memoization[n]
+  print("memo of n ",memoization[n])
+  if n <= 2:
+    return 1
+  print("memo all",memoization)
+  memoization[n] = fib(n-1,memoization)+fib(n-2,memoization)
+  print(" all memo",memoization)
+  return memoization[n]
+
+print(fib(8))
+# print(fib(50))
